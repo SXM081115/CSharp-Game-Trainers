5bfb1baeccb47573de88b5f96ed77b4c301fbc69
SXM081115 <tdf3497995988@gmail.com>
2025-12-06 16:15:30 +0800

    fix BUGs

--- (USE AOB and Hook)Coin Push RPG Trainer/Cheaters/Core/Env.cs
+++ (USE AOB and Hook)Coin Push RPG Trainer/Cheaters/Core/Env.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Cheaters.Core;
 
 public static class Env
--- (USE AOB and Hook)Coin Push RPG Trainer/Cheaters/Windows/MainWindow.xaml.cs
+++ (USE AOB and Hook)Coin Push RPG Trainer/Cheaters/Windows/MainWindow.xaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Cheaters.Windows;
 using System.Windows;
 using System.Windows.Media;
